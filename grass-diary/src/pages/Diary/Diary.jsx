3b3acf10dc5768c0b6ce4ca56c9e779c0c21b6ea
rkdcodus <rcd7275@naver.com>
2024-02-13 00:53:31 +0900

    ♻️ refactor: 컴포넌트 분리 후 적용

--- grass-diary/src/pages/Diary/Diary.jsx
+++ grass-diary/src/pages/Diary/Diary.jsx
@@ -1,11 +1,13 @@
 import * as stylex from "@stylexjs/stylex";
-import testImg from "../../assets/icon/profile.jpeg";
-import { useState } from "react";
+import { useRef } from "react";
+
+import Header from "../../components/Header";
+import BackButton from "../../components/BackButton";
+import Like from "../../components/Like";
+import Title from "./Title";
+import Content from "./Content";
 
 const styles = stylex.create({
-  nav: {
-    height: "80px",
-  },
   wrap: {
     background: "#F9F9F9",
     width: "100vw",
@@ -16,42 +18,8 @@ const styles = stylex.create({
 
     maxWidth: "1200px",
   },
-  button: {
-    backgroundColor: {
-      default: "#F9F9F9",
-      ":hover": "#BFBFBF",
-    },
-    color: {
-      default: "black",
-      ":hover": "white",
-    },
-    border: "1px solid #BFBFBF",
-    width: "36px",
-    height: "36px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    marginRight: "16px",
-  },
-
-  diaryContent: {
-    padding: "16px 16px 100px 16px ",
-  },
-  hashTag: {
-    color: "#28B91C",
-    fontSize: "13px",
-    marginBottom: "36px",
-  },
-  content: {
-    fontSize: "13px",
-    lineHeight: "25px",
-  },
-  like: {
-    marginRight: "20px",
-    fontSize: "30px",
-    textAlign: "center",
-    cursor: "pointer",
-  },
   feelBackground: {
+    marginLeft: "20px",
     width: "40px",
     height: "40px",
     backgroundColor: "#ffffff",
@@ -64,8 +32,8 @@ const styles = stylex.create({
   feel: {
     width: "34px",
     height: "34px",
-    backgroundColor: "#14FF00",
     borderRadius: "50%",
+    backgroundColor: "#D2FBBF",
   },
   diaryFooter: {
     display: "flex",
@@ -74,160 +42,23 @@ const styles = stylex.create({
   },
 });
 
-const viewHeader = stylex.create({
-  progileBox: {
-    position: "relative",
-    width: "50px",
-    height: "50px",
-    margin: "44px 0 28px 0",
-  },
-  profileImg: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    borderRadius: "50%",
-  },
-  emoji: {
-    zIndex: "1",
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    transform: "translate(3px, 3px)",
-  },
-  diaryHeader: {
-    position: "relative",
-    borderBottom: "1px solid #BFBFBF",
-    paddingBottom: "36px",
-    marginBottom: "36px",
-  },
-  title: {
-    fontSize: "40px",
-    fontWeight: "600",
-    marginRight: "24px",
-  },
-  time: {
-    fontSize: "16px",
-    marginRight: "24px",
-  },
-  disclosure: {
-    fontSize: "16px",
-  },
-  ellipsis: {
-    zIndex: "1",
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    marginBottom: "36px",
-    cursor: "pointer",
-  },
-});
-
-const ellipsisStyles = stylex.create({
-  container: {
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    width: "136px",
-    border: "1px solid #BFBFBF",
-    borderRadius: "20px",
-    transform: "translate(20px, 42px)",
-    backgroundColor: "#ffffff",
-  },
-  box: {
-    height: "36px",
-    fontSize: "13px",
-    textAlign: "center",
-    lineHeight: "36px",
-    borderBottom: {
-      default: "1px solid #BFBFBF",
-      ":last-child": "none",
-    },
-  },
-});
-
-const Ellipsis = () => {
-  return (
-    <>
-      <div {...stylex.props(ellipsisStyles.container)}>
-        <div {...stylex.props(ellipsisStyles.box)}></div>
-        <div {...stylex.props(ellipsisStyles.box)}>수정</div>
-        <div {...stylex.props(ellipsisStyles.box)}>삭제</div>
-      </div>
-    </>
-  );
-};
-
 const Diary = () => {
-  const [like, setlike] = useState(false);
-  const [open, setOpen] = useState(false);
-  const clickLike = () => {
-    setlike((current) => !current);
-  };
-
-  const clickEllipsis = () => {
-    setOpen((current) => !current);
-  };
+  const title = "11월 11일 목요일";
+  const time = "23:01";
+  const privateOrPubilc = "비공개";
+  const hashTag = "#해시태그";
+  const content = "오늘은 스터디 회의가 있는 날이었다.";
 
   return (
     <>
-      <div {...stylex.props(styles.nav)}></div>
+      <Header />
       <div {...stylex.props(styles.wrap)}>
-        <div>
-          <button type='button' {...stylex.props(styles.button)}>
-            <i className='fa-solid fa-arrow-left'></i>
-          </button>
-          <span>목록</span>
-        </div>
+        <BackButton />
+        <Title title={title} time={time} privateOrPubilc={privateOrPubilc} />
+        <Content hashTag={hashTag} content={content} />
 
-        <div>
-          <div {...stylex.props(viewHeader.progileBox)}>
-            <img {...stylex.props(viewHeader.profileImg)} src={testImg}></img>
-            <div {...stylex.props(viewHeader.emoji)}>😆</div>
-          </div>
-          <div {...stylex.props(viewHeader.diaryHeader)}>
-            <span {...stylex.props(viewHeader.title)}>11월 11일 목요일</span>
-            <span {...stylex.props(viewHeader.time)}>23:01</span>
-            <span {...stylex.props(viewHeader.disclosure)}>비공개</span>
-            <div onClick={clickEllipsis} {...stylex.props(viewHeader.ellipsis)}>
-              <i className='fa-solid fa-ellipsis-vertical'></i>
-            </div>
-            {open ? <Ellipsis /> : null}
-          </div>
-
-          <div {...stylex.props(styles.diaryContent)}>
-            <div {...stylex.props(styles.hashTag)}>#해시태그 #해시태그 #해시태그</div>
-            <p {...stylex.props(styles.content)}>
-              오늘은 스터디 회의가 있는 날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은
-              스터디 회의가 있는 날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디
-              회의가 있는 날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가
-              있는 날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는
-              날이었다. 스터디 회의를 진행하면서 느낀 점이 있었다. 오늘은 스터디 회의가 있는
-              날이었다.
-              <br></br>
-              <br></br>
-              스터디 회의를 진행하면서 느낀 점이 있었다. 오늘은 스터디 회의가 있는 날이었다. 스터디
-              회의를 진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는 날이었다. 스터디 회의를
-              진행하면서 느낀 점이 있었다.오늘은 스터디 회의가 있는 날이었다. 스터디 회의를
-              진행하면서 느낀 점이 있었다.
-            </p>
-          </div>
-        </div>
         <div {...stylex.props(styles.diaryFooter)}>
-          <div {...stylex.props(styles.like)}>
-            {like ? (
-              <i onClick={clickLike} className='fa-solid fa-heart'></i>
-            ) : (
-              <i onClick={clickLike} className='fa-regular fa-heart'></i>
-            )}
-          </div>
+          <Like />
           <div {...stylex.props(styles.feelBackground)}>
             <div {...stylex.props(styles.feel)}></div>
           </div>
