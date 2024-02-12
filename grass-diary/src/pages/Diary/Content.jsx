24d43816e7ff07241d8f492609d90d2caaa2d15f
rkdcodus <rcd7275@naver.com>
2024-02-13 00:53:05 +0900

    ♻️ refactor:  일기 내용 부분 동일 페이지 폴더 내로 컴포넌트 분리

---
+++ grass-diary/src/pages/Diary/Content.jsx
@@ -0,0 +1,27 @@
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  diaryContent: {
+    padding: "16px 16px 100px 16px ",
+  },
+  hashTag: {
+    color: "#28B91C",
+    fontSize: "13px",
+    marginBottom: "36px",
+  },
+  content: {
+    fontSize: "13px",
+    lineHeight: "25px",
+  },
+});
+
+const Content = ({ hashTag, content }) => {
+  return (
+    <div {...stylex.props(styles.diaryContent)}>
+      <div {...stylex.props(styles.hashTag)}>{hashTag}</div>
+      <p {...stylex.props(styles.content)}>{content}</p>
+    </div>
+  );
+};
+
+export default Content;
