edf441f470a9f107f34ef6a7a3be2a7ab074f02f
rkdcodus <rcd7275@naver.com>
2024-02-13 00:47:40 +0900

    refactor: 뒤로가기 버튼 컴포넌트 분리

---
+++ grass-diary/src/components/BackButton.jsx
@@ -0,0 +1,32 @@
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  button: {
+    backgroundColor: {
+      default: "#F9F9F9",
+      ":hover": "#BFBFBF",
+    },
+    color: {
+      default: "black",
+      ":hover": "white",
+    },
+    border: "1px solid #BFBFBF",
+    width: "36px",
+    height: "36px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    marginRight: "16px",
+  },
+});
+
+const BackButton = () => {
+  return (
+    <div>
+      <button type='button' {...stylex.props(styles.button)}>
+        <i className='fa-solid fa-arrow-left'></i>
+      </button>
+    </div>
+  );
+};
+
+export default BackButton;
