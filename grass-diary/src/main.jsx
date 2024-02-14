0ceecd6a1f5429faa630014ad3306b191636a539
ccconac <qwerpoiu91@naver.com>
2024-02-14 20:36:23 +0900

    🔀 merge: dev branch 백업

--- grass-diary/src/main.jsx
+++ grass-diary/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import './reset.css';
+import '../src/styles/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
