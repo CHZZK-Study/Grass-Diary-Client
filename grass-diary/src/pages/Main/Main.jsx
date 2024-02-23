0049a48ba4a7c2b66a4324ea84e42b10faa90cb5
Yongho Kim <102516350+KKYHH@users.noreply.github.com>
2024-02-23 23:29:09 +0900

    :sparkles: feat: Main,CreateDiary Page 데모 구현 (#6)
    * test: 마이페이지 구현
    
    * chore: react-router-dom 페이지 세팅
    
    * chore: jsx 페이지 세팅
    
    * feat: pretendard 폰트 추가, fontawesome 추가
    
    * docs: public 폴더 image 추가
    
    * feat: main page 레이아웃 middle section 추가
    
    * 🐛 bug: branch conflict 해결
    
    * test
    
    * :lipstick: design: reset CSS 구조 변경
    
    * :sparkles: feat: Main 페이지 카드 슬라이더 추가 (react-slick)
    
    * :lipstick: design: UI 용 div 변경
    
    * :lipstick: design: prettier 추가
    
    * :sparkles: feat: 일기 작성페이지 추가

--- grass-diary/src/pages/Main/Main.jsx
+++ grass-diary/src/pages/Main/Main.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import grass from '../../assets/icon/grass.png';
 import profile from '../../assets/icon/profile.jpeg';
 import mainCharacter from '../../assets/icon/mainCharacter.png';
+import SimpleSlider from './CardSlider';
 
 const styles = stylex.create({
   title: {
@@ -82,32 +83,44 @@ const TopSectionStyles = stylex.create({
     border: 'solid 1px #F9F9F9',
     borderRadius: '30px',
   },
+
   bannerContainer: {
     display: 'flex',
     flexDirection: 'column',
     gap: '30px',
   },
+
   bannerTitle: {
     display: 'flex',
     flexDirection: 'column',
     fontWeight: 'bold',
     fontSize: 25,
   },
+
   writeButton: {
-    backgroundColor: 'white',
+    backgroundColor: {
+      default: 'white',
+      ':hover': 'black',
+    },
+    color: {
+      default: 'black',
+      ':hover': 'white',
+    },
     padding: '10px 20px',
     margin: 0,
     cursor: 'pointer',
-    border: 'solid 1px #E0E0E0',
+    border: 'solid outset 1px #E0E0E0',
     borderRadius: '20px',
     boxShadow: '1px 1px 1px 1px #E0E0E0',
     fontWeight: 'bold',
-    transition: 'background-color 0.5s ease',
+    transition: 'background-color 0.3s ease, color 0.3s ease',
   },
+
   character: {
     width: 300,
     height: 300,
   },
+
   bottomContainer: {
     display: 'flex',
     justifyContent: 'center',
@@ -115,6 +128,7 @@ const TopSectionStyles = stylex.create({
     gap: '200px',
     paddingTop: '50px',
   },
+
   bottomLeftBox: {
     display: 'flex',
     gap: '100px',
@@ -123,6 +137,7 @@ const TopSectionStyles = stylex.create({
     borderRadius: '20px',
     padding: '70px',
   },
+
   bottomLeft: {
     display: 'flex',
     flexDirection: 'column',
@@ -136,6 +151,7 @@ const TopSectionStyles = stylex.create({
     borderRadius: '20px',
     padding: '70px',
   },
+
   bottomRight: {
     display: 'flex',
     flexDirection: 'column',
@@ -147,6 +163,73 @@ const MiddleSectionStyle = stylex.create({
     fontWeight: 'bold',
     fontSize: '30px',
   },
+
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '900px',
+    paddingTop: '50px',
+  },
+
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '300px',
+  },
+
+  grassContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  rewardContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    backgroundColor: {
+      default: 'white',
+      ':hover': 'black',
+    },
+    color: {
+      default: 'black',
+      ':hover': 'white',
+    },
+    marginTop: '30px',
+    padding: '10px 20px',
+    cursor: 'pointer',
+    border: 'solid outset 1px #E0E0E0',
+    borderRadius: '20px',
+    boxShadow: '1px 1px 1px 1px #E0E0E0',
+    fontWeight: 'bold',
+    transition: 'background-color 0.3s ease, color 0.3s ease',
+  },
+});
+
+const BottomSectionStyle = stylex.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '800px',
+    paddingTop: '50px',
+  },
+
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+  },
+
+  text: {
+    fontWeight: 'bold',
+  },
 });
 
 const DropMenu = () => {
@@ -157,17 +240,19 @@ const DropMenu = () => {
           <ul {...stylex.props(styles.ulListNone)}>
             <li
               {...stylex.props(styles.dropBoxEffect)}
-              onClick={(e) => {
+              onClick={e => {
                 e.stopPropagation();
                 console.log('HI');
               }}
             >
               <i className="fa-solid fa-user"></i>
-              <span {...stylex.props(styles.textWithIconLeft)}>마이 페이지</span>
+              <span {...stylex.props(styles.textWithIconLeft)}>
+                마이 페이지
+              </span>
             </li>
             <li
               {...stylex.props(styles.dropBoxEffect)}
-              onClick={(e) => {
+              onClick={e => {
                 e.stopPropagation();
                 console.log('HI');
               }}
@@ -177,7 +262,7 @@ const DropMenu = () => {
             </li>
             <li
               {...stylex.props(styles.dropBoxEffect)}
-              onClick={(e) => {
+              onClick={e => {
                 e.stopPropagation();
                 console.log('HI');
               }}
@@ -195,9 +280,9 @@ const DropMenu = () => {
 const Nav = () => {
   const [showDropMenu, setShowDropMenu] = useState(false);
 
-  const toggleDropMenu = (e) => {
+  const toggleDropMenu = e => {
     e.stopPropagation();
-    setShowDropMenu((prevState) => !prevState);
+    setShowDropMenu(prevState => !prevState);
   };
 
   useEffect(() => {
@@ -214,9 +299,17 @@ const Nav = () => {
   return (
     <>
       <div {...stylex.props(styles.navBar)}>
-        <img src={grass} alt="서비스 로고 아이콘" {...stylex.props(styles.imgNav)} />
+        <img
+          src={grass}
+          alt="서비스 로고 아이콘"
+          {...stylex.props(styles.imgNav)}
+        />
         <div {...stylex.props(styles.dropContainer)}>
-          <img src={profile} alt="사용자 프로필 사진" {...stylex.props(styles.imgNav)} />
+          <img
+            src={profile}
+            alt="사용자 프로필 사진"
+            {...stylex.props(styles.imgNav)}
+          />
           <button {...stylex.props(styles.button)} onClick={toggleDropMenu}>
             <i className="fa-solid fa-list"></i>
           </button>
@@ -233,7 +326,10 @@ const TopSection = () => {
       <div {...stylex.props(TopSectionStyles.container)}>
         <div {...stylex.props(TopSectionStyles.bannerContainer)}>
           <div {...stylex.props(TopSectionStyles.bannerTitle)}>
-            <i className="fa-solid fa-lightbulb" style={{ paddingBottom: '20px' }}></i>
+            <i
+              className="fa-solid fa-lightbulb"
+              style={{ paddingBottom: '20px' }}
+            ></i>
             <div>오늘은</div>
             <div>11월 11일</div>
             <div>빼빼로 데이입니다</div>
@@ -242,7 +338,9 @@ const TopSection = () => {
             <i className="fa-solid fa-circle-question"></i>
             <div>오늘 잠을 얼마나 잤나요?</div>
           </div>
-          <button {...stylex.props(TopSectionStyles.writeButton)}>오늘의 일기 쓰러가기</button>
+          <button {...stylex.props(TopSectionStyles.writeButton)}>
+            오늘의 일기 쓰러가기
+          </button>
         </div>
         <div>
           <img
@@ -276,7 +374,10 @@ const TopSection = () => {
         <div {...stylex.props(TopSectionStyles.bottomRightBox)}>
           <div>
             <button {...stylex.props(styles.button)}>
-              <i className="fa-solid fa-user-group" style={{ fontSize: '50px' }}></i>
+              <i
+                className="fa-solid fa-user-group"
+                style={{ fontSize: '50px' }}
+              ></i>
             </button>
           </div>
           <div {...stylex.props(TopSectionStyles.bottomRight)}>
@@ -307,10 +408,70 @@ const TopSection = () => {
 const MiddleSection = () => {
   return (
     <>
-      <div {...stylex.props(MiddleSectionStyle.text)}>기록 상자</div>
-      <div {...stylex.props(MiddleSectionStyle.text)}>기록 상자</div>
-      <div {...stylex.props(MiddleSectionStyle.text)}>총 2개의 기록을 보유하고 있어요!</div>
-      <div {...stylex.props(MiddleSectionStyle.text)}>총 2개의 기록을 보유하고 있어요!</div>
+      <div {...stylex.props(MiddleSectionStyle.title)}>
+        <div>
+          <h2>기록 상자</h2>
+          <span>총 2개의 기록을 보유하고 있어요!</span>
+        </div>
+        <div></div>
+      </div>
+      <div {...stylex.props(MiddleSectionStyle.container)}>
+        <div
+          className="cardSectionG"
+          {...stylex.props(MiddleSectionStyle.grassContainer)}
+        >
+          <img
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Seedling.png"
+            alt="Seedling"
+            width="125"
+            height="125"
+          />
+          <h2>나의 이번달 잔디</h2>
+          <span>1월 일기는 현재까지 총 15개가 작성되었어요</span>
+          <span>리워드를 확인 해보세요!</span>
+        </div>
+        <div
+          className="cardSectionR"
+          {...stylex.props(MiddleSectionStyle.rewardContainer)}
+        >
+          <img
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Activities/Party%20Popper.png"
+            alt="Party Popper"
+            width="125"
+            height="125"
+          />
+          <h2>나의 리워드</h2>
+          <span>잔디를 꾸준히 심고 리워드를 받으세요</span>
+          <span>테마 상점에서 다양한 아이템을 만날 수 있어요</span>
+          <button {...stylex.props(MiddleSectionStyle.button)}>
+            테마 상점
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const BottomSection = () => {
+  return (
+    <>
+      <div {...stylex.props(BottomSectionStyle.container)}>
+        <div>
+          <div {...stylex.props(BottomSectionStyle.title)}>
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Activities/Trophy.png"
+              alt="Trophy"
+              width="25"
+              height="25"
+            />
+            <h2>이번 주 TOP 10</h2>
+          </div>
+          <span>다른 사람의 하루를 구경하러 가볼까요?</span>
+        </div>
+        <span {...stylex.props(BottomSectionStyle.text)}>
+          더 보러가기 <i className="fa-solid fa-chevron-right"></i>
+        </span>
+      </div>
     </>
   );
 };
@@ -321,6 +482,8 @@ const Main = () => {
       <Nav />
       <TopSection />
       <MiddleSection />
+      <BottomSection />
+      <SimpleSlider />
     </>
   );
 };
