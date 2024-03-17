f5f58e670efd4875b3ccc33d124f76a6264500a0
ccconac <qwerpoiu91@naver.com>
2024-03-18 03:57:25 +0900

    :sparkles: feat: localStorage에서 accessToken 삭제하는 함수 생성

--- grass-diary/src/utils/authUtils.js
+++ grass-diary/src/utils/authUtils.js
@@ -2,3 +2,9 @@ export const checkAuth = () => {
   const accessToken = localStorage.getItem('accessToken');
   return !!accessToken;
 };
+
+const clearAuth = () => {
+  localStorage.removeItem('accessToken');
+};
+
+export { checkAuth, clearAuth };
