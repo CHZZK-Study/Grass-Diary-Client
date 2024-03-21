2e21864051dfd956893874a2822a9cce42bdb87b
ccconac <qwerpoiu91@naver.com>
2024-03-22 04:15:06 +0900

    :sparkles: feat: profile component에 로그인한 사용자 프로필 요청 추가

--- grass-diary/src/components/Profile.jsx
+++ grass-diary/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import defaultProfile from '../assets/icon/basicProfile.png';
+import { useEffect, useState } from 'react';
+import useUser from '../hooks/useUser';
+import API from '../services';
 import stylex from '@stylexjs/stylex';
 
 const styles = stylex.create({
@@ -12,10 +14,25 @@ const styles = stylex.create({
 });
 
 const Profile = ({ width, height }) => {
+  const memberId = useUser();
+  const [profile, setProfile] = useState();
+
+  useEffect(() => {
+    if (memberId) {
+      API.get(`/member/profile/${memberId}`)
+        .then(response => {
+          setProfile(response.data.profileImageURL);
+        })
+        .catch(error => {
+          console.error(`사용자 프로필을 불러올 수 없습니다. ${error}`);
+        });
+    }
+  }, [memberId]);
+
   return (
     <img
       {...stylex.props(styles.profileImage(width, height))}
-      src={defaultProfile}
+      src={profile}
       alt="사용자 프로필 사진"
     />
   );
