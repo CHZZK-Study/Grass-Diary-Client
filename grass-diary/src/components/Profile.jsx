b4f7b71e5bb2a14874121569201830f8a435ac43
ccconac <qwerpoiu91@naver.com>
2024-03-22 04:41:23 +0900

    :recycle: refactor: profile component에 사용자 프로필 조회 custom hook 적용

--- grass-diary/src/components/Profile.jsx
+++ grass-diary/src/components/Profile.jsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react';
-import useUser from '../hooks/useUser';
-import API from '../services';
 import stylex from '@stylexjs/stylex';
+import useProfile from '../hooks/useProfile';
 
 const styles = stylex.create({
   profileImage: (width, height) => ({
@@ -14,25 +12,12 @@ const styles = stylex.create({
 });
 
 const Profile = ({ width, height }) => {
-  const memberId = useUser();
-  const [profile, setProfile] = useState();
-
-  useEffect(() => {
-    if (memberId) {
-      API.get(`/member/profile/${memberId}`)
-        .then(response => {
-          setProfile(response.data.profileImageURL);
-        })
-        .catch(error => {
-          console.error(`사용자 프로필을 불러올 수 없습니다. ${error}`);
-        });
-    }
-  }, [memberId]);
+  const userProfile = useProfile().profileImageURL;
 
   return (
     <img
       {...stylex.props(styles.profileImage(width, height))}
-      src={profile}
+      src={userProfile}
       alt="사용자 프로필 사진"
     />
   );
