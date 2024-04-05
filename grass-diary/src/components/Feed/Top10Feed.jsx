21009bac304b3113365fbfc86b58a9c655a6a0ff
ccconac <qwerpoiu91@naver.com>
2024-04-06 06:46:58 +0900

    🎨 style: stylex import 구문 통일

--- grass-diary/src/components/Feed/Top10Feed.jsx
+++ grass-diary/src/components/Feed/Top10Feed.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import * as stylex from '@stylexjs/stylex';
+import stylex from '@stylexjs/stylex';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
--- grass-diary/src/pages/Diary/Diary.jsx
+++ grass-diary/src/pages/Diary/Diary.jsx
@@ -1,4 +1,4 @@
-import * as stylex from '@stylexjs/stylex';
+import stylex from '@stylexjs/stylex';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import DOMPurify from 'dompurify';
