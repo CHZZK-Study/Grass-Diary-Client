5f1d79b60556c23db901b6c263d4dd1601d1727f
ccconac <qwerpoiu91@naver.com>
2024-04-06 18:46:52 +0900

    :wrench: chore: assets 절대 경로 별칭 수정

--- grass-diary/vite.config.js
+++ grass-diary/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@services': path.resolve(__dirname, 'src/services'),
       '@utils': path.resolve(__dirname, 'src/utils'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@icon': path.resolve(__dirname, 'src/assets/icon'),
     },
   },
 });
