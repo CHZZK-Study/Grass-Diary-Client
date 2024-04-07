57a4c879ba9220f9377ff9a3bbcccc14624625a5
ccconac <qwerpoiu91@naver.com>
2024-04-07 18:19:50 +0900

    :recycle: refactor: useAuth import 경로 변경

--- grass-diary/src/hooks/useUser.js
+++ grass-diary/src/hooks/useUser.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from './useAuth';
+import { useAuth } from '@recoil/auth/useAuth';
 import API from '@services';
 
 const useUser = () => {
