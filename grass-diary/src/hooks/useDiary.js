2f7a00eaadb95eda36bbf24e731f1940f8db4c25
ccconac <qwerpoiu91@naver.com>
2024-04-17 23:34:42 +0900

    :recycle: refactor: 마이 페이지 일기 목록 useQuery 적용

--- grass-diary/src/hooks/useDiary.js
+++ grass-diary/src/hooks/useDiary.js
@@ -1,27 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import API from '@services';
 
-const useDiary = (memberId, currentPage, sortOrder) => {
-  const [diaryList, setDiaryList] = useState([]);
-  const [pageSize, setPageSize] = useState(0);
+const useDiary = ({ memberId, currentPage, sortOrder }) => {
+  const queryKey = ['diaryList', { memberId, currentPage, sortOrder }];
 
-  useEffect(() => {
+  const queryFn = async () => {
     let apiUrl = `/diary/main/${memberId}?page=${currentPage}`;
-    if (sortOrder === 'oldest') {
-      apiUrl += `&sort=createdAt,ASC`;
-    }
 
-    if (memberId) {
-      API.get(apiUrl)
-        .then(response => {
-          setPageSize(response.data.totalPages);
-          setDiaryList(response.data.content);
-        })
-        .catch(error => {
-          console.error(`사용자의 일기를 조회할 수 없습니다. ${error}`);
-        });
-    }
-  }, [memberId, sortOrder, currentPage]);
+    if (sortOrder === 'oldest') apiUrl += `&sort=createdAt,ASC`;
+    const response = await API.get(apiUrl);
+
+    return response.data;
+  };
+
+  const { data: diary } = useQuery({
+    queryKey,
+    queryFn,
+    enabled: !!memberId,
+    onError: error =>
+      console.error(`사용자의 일기를 조회할 수 없습니다. ${error}`),
+  });
+
+  const diaryList = diary?.content || [];
+  const pageSize = diary?.totalPages || 0;
 
   return { diaryList, pageSize };
 };
