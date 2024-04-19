e1777ad721ac77f385cb0d59c0b88cd0f88283d8
ccconac <qwerpoiu91@naver.com>
2024-04-19 11:16:51 +0900

    :recycle: refactor: 사용자 프로필 component type 명시

--- grass-diary/src/components/Profile/Profile.tsx
+++ grass-diary/src/components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import stylex from '@stylexjs/stylex';
 import useProfile from '@recoil/profile/useProfile';
 
 const styles = stylex.create({
-  profileImage: (width, height) => ({
+  profileImage: (width: string, height: string) => ({
     objectFit: 'cover',
     borderRadius: '100%',
 
@@ -11,8 +11,13 @@ const styles = stylex.create({
   }),
 });
 
-const Profile = ({ width, height }) => {
-  const { profileImageURL } = useProfile();
+interface IProfileProps {
+  width: string;
+  height: string;
+}
+
+const Profile = ({ width, height }: IProfileProps) => {
+  const { profileImageURL }: Partial<IProfile> = useProfile();
 
   return profileImageURL ? (
     <img
