32fdf118f7f5601ce10baa33ff86aefac3a09d64
ccconac <qwerpoiu91@naver.com>
2024-04-21 23:10:58 +0900

    :recycle: refactor: ITages 및 IDiary 모든 변수의 type optional 제거

--- grass-diary/src/types/diary.ts
+++ grass-diary/src/types/diary.ts
@@ -1,18 +1,18 @@
 interface ITages {
-  id?: number;
-  tag?: string;
-  tagUsageCount?: number;
+  id: number;
+  tag: string;
+  tagUsageCount: number;
 }
 
 interface IDiary {
-  content?: string;
-  createdAt?: string;
-  createdDate?: string;
-  diaryId?: number;
-  isPrivate?: boolean;
-  likeCount?: number;
-  tags?: ITages[];
-  transparency?: number;
+  content: string;
+  createdAt: string;
+  createdDate: string;
+  diaryId: number;
+  isPrivate: boolean;
+  likeCount: number;
+  tags: ITages[];
+  transparency: number;
 }
 
 // DiaryItem Component Type
