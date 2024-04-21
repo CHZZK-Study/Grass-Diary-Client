e7b38dde77f7f558211169ae2f2123889f4a08e0
ccconac <qwerpoiu91@naver.com>
2024-04-22 04:16:29 +0900

    :recycle: refactor: dateUtils type 명시

--- grass-diary/src/utils/dateUtils.ts
+++ grass-diary/src/utils/dateUtils.ts
@@ -1,17 +1,23 @@
-const formatDate = selectedDate => {
-  const date = new Date(selectedDate);
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
+type TFormatDate = (selectedDate: Date | null) => string;
+
+const formatDate: TFormatDate = selectedDate => {
+  const date: Date = selectedDate ? new Date(selectedDate) : new Date();
+  const month: string = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day: string = date.getDate().toString().padStart(2, '0');
 
   return [month, day].join('/');
 };
 
-const getDaysInYear = year => {
-  const isLeapYear = new Date(year, 1, 29).getMonth() === 1;
+type TGetDaysInYear = (year: number) => number;
+
+const getDaysInYear: TGetDaysInYear = year => {
+  const isLeapYear: boolean = new Date(year, 1, 29).getMonth() === 1;
   return isLeapYear ? 366 : 365;
 };
 
-const getDaysArray = year => {
+type TGetDaysArray = (year: number) => Date[];
+
+const getDaysArray: TGetDaysArray = year => {
   const daysInYear = getDaysInYear(year);
   return Array.from({ length: daysInYear }, (_, i) => new Date(year, 0, i + 1));
 };
