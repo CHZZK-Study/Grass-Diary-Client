091d436c3df75cbbb4bde74dbbbad809833c8fb6
rkdcodus <rcd7275@naver.com>
2024-05-07 22:17:57 +0900

    🔧chore: react-query-devtools 설치

--- grass-diary/src/main.tsx
+++ grass-diary/src/main.tsx
@@ -5,12 +5,14 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './styles/reset.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <RecoilRoot>
         <Suspense>
           <RouterProvider router={router} />
